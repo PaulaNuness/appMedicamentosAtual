1c69aca7e25e5e3711f8087136d20c2703d5ce2f
Paula Nunes <127285238+PaulaNuness@users.noreply.github.com>
2024-02-08 18:47:20 +0100

    08-02__18:47
    jueves

--- lib/utils/validar_pruebas_unitarias.dart
+++ lib/utils/validar_pruebas_unitarias.dart
@@ -1,35 +1,16 @@
 class Validations {
+  //Esse metodo verifica se a string date fornecida está em um formato de data válido. 
+  //RegExp regex = RegExp(r'^([1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$')    Isso define uma expressão regular (regex) que corresponde a um padrão específico de data.A expressão regular r'^([1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$' é dividida em três partes separadas por /.
+  //A primeira parte ([1-9]|[12][0-9]|3[01]) corresponde ao dia do mês. Pode ser qualquer número de 1 a 31.
+  //A segunda parte (0[1-9]|1[0-2]) corresponde ao mês. Pode ser qualquer número de 01 a 12.
+  //A terceira parte \d{4} corresponde ao ano. Deve ter exatamente quatro dígitos e estra entre 1900 e 2100
+  //Ele retorna true se a string corresponder à expressão regular (ou seja, se estiver em um formato de data válido) e false caso contrário.
   bool isDateValid(String date) {
-    RegExp regex =
-        RegExp(r'^([1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$');
-    return regex.hasMatch(date);
-  }
+  RegExp regex =
+      RegExp(r'^([1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(19\d{2}|20\d{2}|2100)$');
+  return regex.hasMatch(date);
+}
 
-  bool isMonthValid(String date) {
-    List<String> dateParts = date.split('/');
-    if (dateParts.length == 3) {
-      try {
-        int month = int.parse(dateParts[1]);
-        return month >= 1 && month <= 12;
-      } catch (e) {
-        return false;
-      }
-    }
-    return false;
-  }
-
-  bool isYearValid(String date) {
-    List<String> dateParts = date.split('/');
-    if (dateParts.length == 3) {
-      try {
-        int year = int.parse(dateParts[2]);
-        return year >= 1900 && year <= 2100;
-      } catch (e) {
-        return false;
-      }
-    }
-    return false;
-  }
 
   bool isSexValid(String sex) {
     return ['hombre', 'mujer'].contains(sex.toLowerCase());
--- test/pruebas_unitarias_validador.dart
+++ test/pruebas_unitarias_validador.dart
@@ -3,15 +3,34 @@ import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
+
+  // Expected: <true>
+  // Actual: <false> porque o ano nao esta entre 1900 e 2100
+//test('isDateValid should return true for a valid date', () {
+      //final validations = Validations();
+      //expect(validations.isDateValid('31/01/0000'), true);
+    //});
+
+
+  // All tests passed! porque o ano esta entre 1900 e 2100
 test('isDateValid should return true for a valid date', () {
       final validations = Validations();
-      expect(validations.isDateValid('31/01/2022'), true);
+      expect(validations.isDateValid('31/01/2024'), true);
     });
 
-test('isSexValid should return true for a valid sex', () {
-      final validations = Validations();
-      expect(validations.isSexValid('hombre'), true);
-    });
+
+
+//test('isSexValid should return true for a valid sex', () {
+      //final validations = Validations();
+      //expect(validations.isSexValid('hombre'), true);
+    //});
+
+
+
+//test('isSexValid should return true for a valid sex', () {
+      //final validations = Validations();
+      //expect(validations.isSexValid('gato'), true);
+    //});
 
 
 }
